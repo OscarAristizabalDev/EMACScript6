a1c4fd692529c45f730e48861fe2d91b399ce06c
OscarAristizabalDev <oaristizabal@dataifx.com>
2022-08-16 16:24:11 -0500

    Se agregan comentarios

--- prototipos/01-Object-getPrototypeOf.js
+++ prototipos/01-Object-getPrototypeOf.js
@@ -20,7 +20,7 @@ console.log(Object.getPrototypeOf(angora) === gato);
 angora.sonido();
 angora.chillido();
 
-
+// ES6 setPrototypeOf permite trabaja con herencia en JavaScript
 Object.setPrototypeOf(angora, perro); // se incluye en ES6
 console.log(Object.getPrototypeOf(angora) === gato);
 angora.sonido();
