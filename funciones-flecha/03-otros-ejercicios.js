8f846965d409f83f83a5f8e70a7690cfdbe120ac
OscarAristizabalDev <oaristizabal@dataifx.com>
2022-08-16 11:49:00 -0500

    Se realiza codigo donde se demuestra que una funciones de flecha no se puede instanciar y no tiene arguments

--- funciones-flecha/03-otros-ejercicios.js
+++ funciones-flecha/03-otros-ejercicios.js
@@ -26,3 +26,20 @@ var manejador2 = {
 }
 
 manejador2.init()
+
+
+
+////
+
+var restar = (a, b) => a - b;
+console.log(typeof restar); // muestra de que tipo es restar
+console.log(restar instanceof Function); // devuelve true o false si es una instacion de una funcion
+
+
+// var restar2 = new restar(1, 2) // generar error porque una funcion de flecha no tiene constructor
+// var restar2 = restar(1, 2) // funciona correctamente
+
+
+((a, b) => {
+    console.log(arguments[0]) // las funciones de flecha no maneja arguments
+})();
