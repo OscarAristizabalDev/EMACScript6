7589836ed7d053cc1dfdc4c6876dff4dd431b5d0
OscarAristizabalDev <oaristizabal@dataifx.com>
2022-08-19 07:44:18 -0500

    Iniciacializando maps con valores por defecto

---
+++ maps/02-inicializando-con-valores.js
@@ -0,0 +1,8 @@
+// inicializando un mapa con valores por defecto
+// un unico arreglo, con arreglos de los valores
+let mapa = new Map([
+    ["nombre", "fernando"],
+    ["edad", 31]
+]);
+
+console.log(mapa);
