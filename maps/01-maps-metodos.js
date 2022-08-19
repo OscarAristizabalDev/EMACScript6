c2823623d0b9c92e5bd4b796d83fa0af8db70cbf
OscarAristizabalDev <oaristizabal@dataifx.com>
2022-08-19 07:40:40 -0500

    Maps : metodos, set, get, has, delete, clear

---
+++ maps/01-maps-metodos.js
@@ -0,0 +1,33 @@
+let mapa = new Map();
+
+// añadir datos
+mapa.set("nombre", "Fernando");
+mapa.set("edad", 31);
+mapa.set("apellido"); // al no asignar ningun valor queda como undefined
+
+console.log(mapa);
+console.log(mapa.size);
+
+// obtener datos
+console.log(mapa.get("nombre"));
+console.log(mapa.get("edad"));
+
+// Validar si el dato exite
+console.log(mapa.has("nombre")); // true or false
+console.log(mapa.has("edad")); // true or false
+console.log(mapa.has("apellido")); // true or false
+
+// eliminar un llave
+mapa.delete("nombre");
+console.log(mapa.has("nombre")); // true or false
+console.log(mapa.get("nombre"));
+
+
+// eliminar solo el valor y uno la llave
+mapa.set("edad"); // la edad queda como undefined
+console.log(mapa);
+
+
+// limpiar todo el mapa 
+mapa.clear();
+console.log(mapa);
