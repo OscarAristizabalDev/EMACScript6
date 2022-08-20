528865c8249665990ae5edd4e50d8fa8599f2bd6
OscarAristizabalDev <oaristizabal@dataifx.com>
2022-08-20 11:32:17 -0500

    Problemas con el manejo de callback

---
+++ asincrono/02-callback-problemas.js
@@ -0,0 +1,60 @@
+const empleados = [{
+        id: 1,
+        nombre: 'Oscar'
+    }, {
+        id: 2,
+        nombre: 'Andres'
+    },
+    {
+        id: 3,
+        nombre: 'Felipe'
+    }
+];
+
+const salarios = [{
+    id: 1,
+    salario: 1000
+}, {
+    id: 2,
+    salario: '2000'
+}];
+
+const getEmpleado = (id, callback) => {
+
+    const empleado = empleados.find(emp => emp.id === id);
+    if (!empleado) {
+        callback(`No existe empleado con el id ${id}`);
+    } else {
+        callback(null, empleado);
+    }
+};
+
+const getSalario = (empleado, callback) => {
+    const salarioEmpledo = salarios.find(salario => salario.id === empleado.id);
+    if (!salarioEmpledo) {
+        callback(`No existe el salario del empleado con el id ${empleado.id}`);
+    } else {
+        callback(null, {
+            nombre: empleado.nombre,
+            salario: salarioEmpledo.salario,
+            id: empleado.id
+        });
+    }
+}
+
+// Uso
+getEmpleado(2, (err, empleado) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    // El problema con los callback es que se empieza a crecer cada vez que se hagan llamados
+    getSalario(empleado, (err, salarioEmpleado) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('detalle empleado', salarioEmpleado);
+    });
+    //console.log('emppleado', empleado);
+
+});
